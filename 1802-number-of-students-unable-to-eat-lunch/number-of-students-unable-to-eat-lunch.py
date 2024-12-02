8d323bcd186111b4af12f42afbaec4546f833e71
Yushi Cui <qs8cys@gmail.com>
2024-12-02 23:03:17 +1300

    Time: 3 ms (18.95%) | Memory: 17.6 MB (5.50%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,17 +1,12 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         res=len(students)
-        sm={}
-
-        for i in students:
-            sm[i]=sm.get(i,0)+1
-        for sw in sandwiches:
-            if sm.get(sw,0)>0:
+        studentMap=Counter(students)
+        for s in sandwiches:
+            if studentMap[s]>0:
                 res-=1
-                sm[sw]-=1
+                studentMap[s]-=1
             else:
                 return res
         return res
-                
-        
         
