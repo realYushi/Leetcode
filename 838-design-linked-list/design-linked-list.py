1702834021d7e8352b8165c33741a8004ac8d4c9
Yushi Cui <qs8cys@gmail.com>
2024-12-02 16:34:41 +1300

    Time: 91 ms (7.24%) | Memory: 18.3 MB (9.53%) - LeetSync

--- 838-design-linked-list/design-linked-list.py
+++ 838-design-linked-list/design-linked-list.py
@@ -9,10 +9,10 @@ class MyLinkedList:
         self.left.next=self.right
         self.size=0
     def get(self, index: int) -> int:
-        if index>=self.size or index < 0 :
+        if index >= self.size or index<0:
             return -1
-        curr=self.left.next
-        for _ in range(index):
+        curr = self.left.next
+        for _ in range(index ):
             curr=curr.next
         return curr.val
     def addAtHead(self, val: int) -> None:
@@ -22,43 +22,37 @@ class MyLinkedList:
         self.size+=1
     def addAtTail(self, val: int) -> None:
         node = LinkedList(val)
-        curr = self.left
-        while curr.next!=self.right:
+        curr=self.left
+        while curr.next != self.right:
             curr=curr.next
         node.next=self.right
         curr.next=node
         self.size+=1
-        
-
     def addAtIndex(self, index: int, val: int) -> None:
-        if index > self.size or index < 0 :
+        if index > self.size or index <0:
             return 
-        if index == 0:
+        if index ==0:
             self.addAtHead(val)
-            return
-        if  index == self.size:
+            return 
+        if index == self.size:
             self.addAtTail(val)
             return
+        curr=self.left
         node = LinkedList(val)
-        curr = self.left
         for _ in range(index):
             curr=curr.next
-        node.next=curr.next
+        node.next =curr.next
         curr.next=node
         self.size+=1
-
-
-        
-
-
     def deleteAtIndex(self, index: int) -> None:
-        if index >= self.size or index<0 :
+        if index >= self.size or index <0:
             return 
-        curr = self.left
-        for _ in range(index):
+        curr  = self.left
+        for  _ in range(index):
             curr=curr.next
-        curr.next = curr.next.next
+        curr.next=curr.next.next
         self.size-=1
+
         
 
 
