178dbb57c3a8bb3778238fc33940869623911390
Yushi Cui <qs8cys@gmail.com>
2024-12-02 13:37:42 +1300

    Time: 97 ms (5.78%) | Memory: 18.1 MB (35.48%) - LeetSync

--- 838-design-linked-list/design-linked-list.py
+++ 838-design-linked-list/design-linked-list.py
@@ -1,67 +1,61 @@
 class LinkedList:
-    def  __init__(self,val):
-        self.val = val
+    def __init__(self, val=0):
         self.next = None
+        self.val = val
+
+
 class MyLinkedList:
     def __init__(self):
-        self.left=LinkedList(0)
-        self.right=LinkedList(0)
-        self.left.next=self.right
-        self.size=0
+        self.left = LinkedList()  # Sentinel node for the head
+        self.right = LinkedList()  # Sentinel node for the tail
+        self.left.next = self.right
+        self.size = 0
 
     def get(self, index: int) -> int:
-        if index<0 or index >=self.size:
+        if index < 0 or index >= self.size:
             return -1
-        curr=self.left.next
+        curr = self.left.next
         for _ in range(index):
-            curr=curr.next
+            curr = curr.next
         return curr.val
+
     def addAtHead(self, val: int) -> None:
-        node=LinkedList(val)
-        node.next=self.left.next
-        self.left.next=node
-        self.size+=1
+        node = LinkedList(val)
+        node.next = self.left.next
+        self.left.next = node
+        self.size += 1
+
     def addAtTail(self, val: int) -> None:
-        node=LinkedList(val)
-        curr=self.left
+        node = LinkedList(val)
+        curr = self.left
         while curr.next != self.right:
-            curr=curr.next 
-        node.next=self.right
-        curr.next=node
-        self.size+=1
+            curr = curr.next
+        node.next = self.right
+        curr.next = node
+        self.size += 1
+
     def addAtIndex(self, index: int, val: int) -> None:
-        if index <0 or index > self.size:
-            return 
+        if index < 0 or index > self.size:
+            return
         if index == 0:
             self.addAtHead(val)
             return
-        if index ==self.size:
+        if index == self.size:
             self.addAtTail(val)
-            return 
-        node = LinkedList(val)
-        curr=self.left.next
-        for _ in range(index-1):
-            curr=curr.next
-        node.next=curr.next
-        curr.next=node
-        self.size+=1
-    def deleteAtIndex(self, index: int) -> None:
-        if index < 0 or index >=self.size:
             return
-        curr=self.left
+        node = LinkedList(val)
+        curr = self.left
         for _ in range(index):
-            curr=curr.next
-        curr.next=curr.next.next
-        self.size-=1
+            curr = curr.next
+        node.next = curr.next
+        curr.next = node
+        self.size += 1
 
-
-        
-
-
-# Your MyLinkedList object will be instantiated and called as such:
-# obj = MyLinkedList()
-# param_1 = obj.get(index)
-# obj.addAtHead(val)
-# obj.addAtTail(val)
-# obj.addAtIndex(index,val)
-# obj.deleteAtIndex(index)
+    def deleteAtIndex(self, index: int) -> None:
+        if index < 0 or index >= self.size:
+            return
+        curr = self.left
+        for _ in range(index):
+            curr = curr.next
+        curr.next = curr.next.next
+        self.size -= 1
