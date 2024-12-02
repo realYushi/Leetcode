442ea6c0eefebb878b5eeec85c1ff2ec1627270c
Yushi Cui <qs8cys@gmail.com>
2024-12-02 16:48:38 +1300

    Time: 10 ms (29.76%) | Memory: 20.9 MB (14.51%) - LeetSync

--- 155-min-stack/min-stack.py
+++ 155-min-stack/min-stack.py
@@ -1,10 +1,10 @@
 class MinStack:
 
     def __init__(self):
-        self.mStack=[]
         self.stack=[]
+        self.mStack=[]
     def push(self, val: int) -> None:
-        mVal=min(val,self.mStack[-1] if self.stack else val)
+        mVal=min(val,self.mStack[-1] if self.mStack else val)
         self.stack.append(val)
         self.mStack.append(mVal)
     def pop(self) -> None:
@@ -12,8 +12,6 @@ class MinStack:
         self.mStack.pop()
     def top(self) -> int:
         return self.stack[-1]
-        
-
     def getMin(self) -> int:
         return self.mStack[-1]
         
