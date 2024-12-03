eafe7eba2872c52cb3161303335767e2a91c4e6a
Yushi Cui <qs8cys@gmail.com>
2024-12-03 20:59:19 +1300

    Time: 29 ms (76.69%) | Memory: 19.6 MB (5.05%) - LeetSync

--- 1582-design-browser-history/design-browser-history.py
+++ 1582-design-browser-history/design-browser-history.py
@@ -1,8 +1,7 @@
 class BrowserHistory:
-
     def __init__(self, homepage: str):
         self.history=[homepage]
-        self.index = 0
+        self.index=0
     def visit(self, url: str) -> None:
         self.history=self.history[:self.index+1]
         self.history.append(url)
@@ -10,6 +9,8 @@ class BrowserHistory:
     def back(self, steps: int) -> str:
         self.index=max(0,self.index-steps)
         return self.history[self.index]
+        
+
     def forward(self, steps: int) -> str:
         self.index=min(len(self.history)-1,self.index+steps)
         return self.history[self.index]
