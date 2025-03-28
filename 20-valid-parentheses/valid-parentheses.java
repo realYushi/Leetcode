08b9d44c96a308cfdc8e072e5f5636f1bb266e5e
Yushi Cui <qs8cys@gmail.com>
2025-03-29 11:22:47 +1300

    Time: 2 ms (97.57%) | Memory: 41.8 MB (47.26%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(char c : s.toCharArray() ){
+        for(char c : s.toCharArray()){
             if(c=='('){
                 stack.push(')');
-            }else if(c=='['){
-                stack.push(']');
-
-            }
-            else if(c=='{'){
+            }else if(c=='{'){
                 stack.push('}');
             }
-            else if(stack.isEmpty()||c!=stack.pop()){
+            else if (c=='['){
+                stack.push(']');
+            }else if(stack.isEmpty()||stack.pop()!=c){
                 return false;
             }
-
         }
-        return stack.isEmpty();
-        
+            return stack.isEmpty();
     }
 }
