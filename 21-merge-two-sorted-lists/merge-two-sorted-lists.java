10d4a63ca68e483fdeb6c14af6e0250b7382ffdd
Yushi Cui <qs8cys@gmail.com>
2025-03-31 10:01:20 +1300

    Time: 0 ms (100.00%) | Memory: 42.7 MB (44.82%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -11,7 +11,7 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode dummyHead=new ListNode();
-        ListNode curr= dummyHead;
+        ListNode curr=dummyHead;
         while(list1!=null&&list2!=null){
             if(list1.val<list2.val){
                 curr.next=list1;
@@ -23,10 +23,10 @@ class Solution {
             curr=curr.next;
         }
         if(list1==null){
-           curr.next=list2; 
-        }else {
+            curr.next=list2;
+        }else{
             curr.next=list1;
-        } 
+        }
         return dummyHead.next;
         
     }
