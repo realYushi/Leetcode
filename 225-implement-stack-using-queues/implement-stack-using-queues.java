e9fd755b38f039d74a1eb4204d05f092c578c0b7
Yushi Cui <qs8cys@gmail.com>
2025-03-31 11:07:58 +1300

    Time: 0 ms (100.00%) | Memory: 41.4 MB (64.42%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -6,23 +6,25 @@ class MyStack {
     }
     
     public void push(int x) {
-        q.offer(x);
-        for(int i =0; i<q.size()-1;i++){
-            q.offer(q.poll());
+        this.q.offer(x);
+        for(int i=0; i<q.size()-1;i++){
+            this.q.offer(q.poll());
         }
+        
     }
     
     public int pop() {
-        return q.poll();
+        return this.q.poll();
+        
     }
     
     public int top() {
-        return q.peek();
+        return this.q.peek();
         
     }
     
     public boolean empty() {
-        return q.isEmpty();
+        return this.q.isEmpty();
         
     }
 }
