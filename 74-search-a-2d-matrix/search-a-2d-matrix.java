39b67a65cfff3c8edf2ebd9cfa892fd85f1bcc5b
Yushi Cui <qs8cys@gmail.com>
2025-04-10 16:56:38 +1200

    Time: 0 ms (100.00%) | Memory: 41.9 MB (96.60%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,41 +1,37 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int ROWS = matrix.length;
-        int COLS = matrix[0].length;
-
-        int l = 0;
-        int r = ROWS - 1;
-        int row = -1;
-        while (l <= r) {
-            
-            int mid= (l + r) / 2;
-            if (matrix[mid][0] <= target && matrix[mid][COLS - 1] >= target) {
-                row = mid;
+        int ROWS=matrix.length;
+        int COLS=matrix[0].length;
+        int l=0;
+        int r=ROWS-1;
+        int row=-1;
+        while(l<=r){
+            int m= (l+r)/2;
+            if(matrix[m][0]<=target&&matrix[m][COLS-1]>=target){
+                row=m;
                 break;
-            } else if (matrix[mid][0] > target) {
-                r = mid - 1;
-            } else {
-                l = mid + 1;
+            }else if(matrix[m][0]>target){
+                r=m-1;
+            }else{
+                l=m+1;
             }
         }
-        if (row==-1) {
+        if(row==-1){
             return false;
         }
         l=0;
         r=COLS-1;
         while(l<=r){
-            int m=(l+r)/2;
-            if(matrix[row][m]>target){
-                r=m-1;
-            }
-            else if(matrix[row][m]<target){
+            int m= (l+r)/2;
+            if(matrix[row][m]<target){
                 l=m+1;
-            }
-            else{
+            }else if(matrix[row][m]>target){
+                r=m-1;
+            }else{
                 return true;
             }
         }
         return false;
-
+        
     }
 }
