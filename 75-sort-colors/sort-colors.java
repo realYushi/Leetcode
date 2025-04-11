8cc0346f6015f63e24a76bf4147f6aac1cd1f993
Yushi Cui <qs8cys@gmail.com>
2025-04-11 18:40:03 +1200

    Time: 0 ms (100.00%) | Memory: 42.6 MB (5.20%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,17 +1,21 @@
 class Solution {
     public void sortColors(int[] nums) {
+       
         int[] bucket=new int[]{0,0,0};
-        for(int i=0; i<nums.length;i++){
-            bucket[nums[i]]++;
+        for(int i:nums){
+            bucket[i]++;
         }
-        int j=0;
-        for(int i=0; i< bucket.length;i++){
+        int k=0;
+        for(int i=0; i<bucket.length;i++){
             while(bucket[i]>0){
-                nums[j]=i;
-                j++;
+                nums[k]=i;
+                k++;
                 bucket[i]--;
             }
         }
+        
+            
+
         
     }
 }
