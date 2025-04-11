ac6db7db9efc4aa3527cb91e894f9cbaffadf678
Yushi Cui <qs8cys@gmail.com>
2025-04-11 20:08:21 +1200

    Time: 0 ms (100.00%) | Memory: 40.6 MB (38.70%) - LeetSync

--- 374-guess-number-higher-or-lower/guess-number-higher-or-lower.java
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.java
@@ -11,9 +11,8 @@ public class Solution extends GuessGame {
     public int guessNumber(int n) {
         int l=0;
         int r=n;
-        
         while(l<=r){
-            int m=l+(r-l)/2;
+            int m= l+(r-l)/2;
             int res=guess(m);
             if(res==1){
                 l=m+1;
@@ -22,7 +21,9 @@ public class Solution extends GuessGame {
             }else{
                 return m;
             }
+
         }
         return -1;
+        
     }
 }
