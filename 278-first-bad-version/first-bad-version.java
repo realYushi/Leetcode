4a9f812ae8df11818e01396aa6db979db79178c9
Yushi Cui <qs8cys@gmail.com>
2025-04-17 22:49:08 +1200

    Time: 25 ms (71.52%) | Memory: 40.8 MB (17.86%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -3,20 +3,21 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int l=1;
+        int l=0;
         int r=n;
-        int res=-1;
+        int res=n;
         while(l<=r){
             int m=l+(r-l)/2;
-            if(isBadVersion(m)){
+            boolean isBad=isBadVersion(m);
+            if(!isBad){
+                l=m+1;
+            }else{
                 res=m;
                 r=m-1;
-            }
-            else{
-                l=m+1;
             }
         }
         return res;
         
+
     }
 }
