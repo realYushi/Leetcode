ebdce4142be1d8343f0e31214f72c8d095e4b210
Yushi Cui <qs8cys@gmail.com>
2025-04-20 10:11:24 +1200

    Time: 53 ms (40.57%) | Memory: 52.3 MB (6.72%) - LeetSync

--- 1582-design-browser-history/design-browser-history.java
+++ 1582-design-browser-history/design-browser-history.java
@@ -1,35 +1,33 @@
 class BrowserHistory {
-    List<String> list;
-    int size;
+    List<String> history;
     int i;
+    int size;
 
     public BrowserHistory(String homepage) {
-        this.list=new ArrayList<>();
-        this.list.add(homepage);
-        this.size=1;
+        this.history=new ArrayList<>();
+        this.history.add(homepage);
         this.i=0;
-        
+        this.size=1;
     }
     
     public void visit(String url) {
         this.i++;
-        if(this.i>=this.list.size()){
-            this.list.add(url);
+        if(this.i==this.history.size()){
+            this.history.add(url);
         }else{
-            this.list.set(this.i,url);
+            this.history.set(this.i,url);
         }
         this.size=this.i+1;
-        
     }
     
     public String back(int steps) {
         this.i=Math.max(0,this.i-steps);
-        return this.list.get(this.i);
+        return this.history.get(this.i);
     }
     
     public String forward(int steps) {
         this.i=Math.min(this.size-1,this.i+steps);
-        return this.list.get(this.i);
+        return this.history.get(this.i);
         
     }
 }
