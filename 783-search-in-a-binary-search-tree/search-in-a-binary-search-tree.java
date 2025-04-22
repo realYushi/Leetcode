6885f24c9e21610a558690d16cb4ea6898b13f65
Yushi Cui <qs8cys@gmail.com>
2025-04-23 08:32:21 +1200

    Time: 0 ms (100.00%) | Memory: 45.5 MB (31.00%) - LeetSync

--- 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ 783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -15,15 +15,19 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root==null||root.val==val){
+        if(root==null){
+            return null;
+        }
+        if(root.val==val){
             return root;
         }
         if(val>root.val){
             return searchBST(root.right,val);
-        } else{
+        }
+        if(val<root.val){
             return searchBST(root.left,val);
         }
-
+        return null;
         
     }
 }
