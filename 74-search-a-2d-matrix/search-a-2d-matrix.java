524c7d8d5403355f76e17632978d277c211206e2
Yushi Cui <qs8cys@gmail.com>
2025-05-01 14:26:05 +1200

    Time: 0 ms (100.00%) | Memory: 41.7 MB (98.62%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,37 +1,48 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int ROWS=matrix.length;
-        int COLS=matrix[0].length;
+    public int bs1(int[][] matrix,int target){
         int l=0;
-        int r=ROWS-1;
-        int row=-1;
+        int r=matrix.length-1;
+        int row=matrix[0].length-1;
         while(l<=r){
             int m=l+(r-l)/2;
-            if(target>=matrix[m][0]&&target<=matrix[m][COLS-1]){
-                row=m;
-                break;
-            }else if(matrix[m][0]<target){
+            if(target>matrix[m][row]){
                 l=m+1;
-            }else{
+            }else if(target<matrix[m][0]){
                 r=m-1;
+            }else{
+                return m;
             }
         }
+        return -1;
+    }
+    public int bs2(int[] list,int target){
+        int l=0;
+        int r=list.length-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(target>list[m]){
+                l=m+1;
+            }else if(target<list[m]){
+                r=m-1;
+            }else{
+                return m;
+            }
+        }
+        return -1;
+
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row=bs1(matrix,target);
         if(row==-1){
             return false;
         }
-        l=0;
-        r=COLS;
-        while(l<=r){
-            int m=l+(r-l)/2;
-            if(matrix[row][m]<target){
-                l=m+1;
-            }else if(matrix[row][m]>target){
-                r=m-1;
-            }else{
-                return true;
-            }
-        }
+        int res=bs2(matrix[row],target);
 
-    return false;
+        return !(res==-1);
+
+        
+        
+
+        
     }
 }
