3679062b1bd76dd2e8710fbe2b13bf138b6afe8f
Yushi Cui <qs8cys@gmail.com>
2025-05-04 14:52:38 +1200

    Time: 0 ms (100.00%) | Memory: 43.1 MB (10.33%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,21 +1,19 @@
 class Solution {
-    public void dfs(int[] nums, int i, List<List<Integer>> res,List<Integer> sub ){
+    public void bk(List<List<Integer>> res, List<Integer> sub, int i, int[] nums){
         if(i>=nums.length){
             res.add(new ArrayList<>(sub));
             return;
         }
         sub.add(nums[i]);
-        dfs(nums,i+1,res,sub);
-        
+        bk(res,sub,i+1,nums);
         sub.remove(sub.size()-1);
-        dfs(nums,i+1,res,sub);
+        bk(res,sub,i+1,nums);
     }
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> res= new ArrayList<>();
-        List<Integer> sub= new ArrayList<>();
-        dfs(nums,0,res,sub);
+        List<List<Integer>> res=new ArrayList<>();
+        List<Integer> sub=new ArrayList<>();
+        bk(res,sub,0,nums);
         return res;
-
         
     }
 }
