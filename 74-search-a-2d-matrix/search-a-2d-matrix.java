dc2c92df776d3449740673fd6ee99a2e0f587efa
Yushi Cui <qs8cys@gmail.com>
2025-05-05 16:47:18 +1200

    Time: 0 ms (100.00%) | Memory: 42.4 MB (27.36%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,8 +1,8 @@
 class Solution {
-    public int bs1(int[][]matrix,int target){
+    public int bs1(int[][] matrix, int target){
+        int rowLen=matrix[0].length-1;
         int l=0;
         int r=matrix.length-1;
-        int rowLen=matrix[0].length-1;
         while(l<=r){
             int m=l+(r-l)/2;
             if(target<matrix[m][0]){
@@ -10,38 +10,34 @@ class Solution {
             }else if(target>matrix[m][rowLen]){
                 l=m+1;
             }else{
-                return m;
+            return m;
+
             }
         }
         return -1;
     }
-    public int bs2(int[] list,int target){
+    public boolean bs2(int[] list, int target){
         int l=0;
         int r=list.length-1;
         while(l<=r){
-            int m=l+(r-l)/2;
-            if(target>list[m]){
-                l=m+1;
-            }else if(target<list[m]){
+            int m= l+ (r-l)/2;
+            if(target<list[m]){
                 r=m-1;
+            }else if(target>list[m]){
+                l=m+1;
             }else{
-                return m;
+                return true;
             }
-
         }
-        return -1;
+        return false;
     }
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row=bs1(matrix,target);
+        int row = bs1(matrix,target);
         if(row==-1){
-            return false;
+            return false; 
         }
-        int index=bs2(matrix[row],target);
-        if(index==-1){
-            return false;
-        }
-        return true;
-
+        return bs2(matrix[row],target);
+        
         
     }
 }
