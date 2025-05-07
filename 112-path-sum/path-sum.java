ec0e13a1d742f4f27c07d78bac5c89f22858e0e4
Yushi Cui <qs8cys@gmail.com>
2025-05-07 15:27:30 +1200

    Time: 0 ms (100.00%) | Memory: 42.9 MB (95.46%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -17,8 +17,9 @@ class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root==null){
             return false;
+
         }
-        targetSum-=root.val;
+        targetSum-= root.val;
         if(root.left==null&&root.right==null){
             return targetSum==0;
         }
