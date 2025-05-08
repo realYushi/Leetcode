e116d9eb84b4d3e943fac9ee71a7bbbdaa9a14e0
Yushi Cui <qs8cys@gmail.com>
2025-05-08 14:41:11 +1200

    Time: 7 ms (9.69%) | Memory: 84.4 MB (5.18%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,22 +15,23 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if(preorder.length==0||inorder.length==0){
+        if(inorder.length==0||preorder.length==0){
             return null;
         }
         TreeNode root= new TreeNode(preorder[0]);
-        int m=-1; 
+        int m=-1;
         for(int i=0; i<inorder.length;i++){
             if(inorder[i]==root.val){
                 m=i;
             }
         }
+
         int[] lpreorder=Arrays.copyOfRange(preorder,1,m+1);
         int[] linorder=Arrays.copyOfRange(inorder,0,m);
         root.left=buildTree(lpreorder,linorder);
-        
-        int[] rpreorder=Arrays.copyOfRange(preorder,m+1,preorder.length);
-        int[] rinorder=Arrays.copyOfRange(inorder,m+1,preorder.length);
+
+        int[] rpreorder=Arrays.copyOfRange(preorder,m+1,inorder.length);
+        int[] rinorder=Arrays.copyOfRange(inorder,m+1,inorder.length);
         root.right=buildTree(rpreorder,rinorder);
         
         return root;
