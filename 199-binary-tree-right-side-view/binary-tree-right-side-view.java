4223422e64e35976774290720ad1e0e8389495a3
Yushi Cui <qs8cys@gmail.com>
2025-05-13 07:41:39 +1200

    Time: 1 ms (71.38%) | Memory: 42.2 MB (43.37%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.java
@@ -14,32 +14,29 @@
  * }
  */
 class Solution {
-    public void bfs(TreeNode root, List<Integer> res){
-        Queue<TreeNode> q= new LinkedList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+            return list;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        while(!q.isEmpty()){
-            int len=q.size();
-            TreeNode last=null;
-            for(int i=0;i<len;i++){
-                TreeNode node=q.remove();
-                if(node.left!=null){
+        while (q.size() > 0) {
+            int len = q.size();
+            TreeNode t=null;
+            for (int i = 0; i < len; i++) {
+                TreeNode node = q.remove();
+                if (node.left != null) {
                     q.add(node.left);
                 }
-                if(node.right!=null){
+                if (node.right != null) {
                     q.add(node.right);
                 }
-                last=node;
+                t=node;
             }
-            res.add(last.val);
+            list.add(t.val);
         }
-    }
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> res=new ArrayList<>();
-        if(root==null){
-            return res;
-        }
-        bfs(root,res);
-        return res;
-        
+        return list;
+
     }
 }
