81ff1d10cbe515c3ad0aabee56416716b61044d4
Yushi Cui <qs8cys@gmail.com>
2025-05-14 20:08:30 +1200

    Time: 4 ms (49.17%) | Memory: 52.7 MB (10.59%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,38 +3,35 @@ class Solution {
         int ROWS=grid.length;
         int COLS=grid[0].length;
         int res=0;
-        for(int r=0; r<ROWS;r++){
+        for(int r=0;r<ROWS;r++){
             for(int c=0; c<COLS; c++){
                 if(grid[r][c]=='1'){
-
-                bfs(grid,r,c);
-                res++;
+                    bfs(grid,r,c);
+                    res++;
                 }
             }
         }
         return res;
     }
-    private void bfs(char [][] grid, int r, int c){
-        Queue<int[]> q = new LinkedList<>();
+    private void bfs(char[][] grid, int r, int c){
+        Queue<int[]> q= new LinkedList<>();
         q.add(new int[]{r,c});
         while(!q.isEmpty()){
-            int[] p=q.remove();
+            int[] p= q.remove();
             int row=p[0];
             int col=p[1];
-            for(int[] d:dir){
+            for(int[] d:dirs){
                 int nr=row+d[0];
                 int nc=col+d[1];
-                if(nr>=0&&nc>=0&&
-                    nr<grid.length&&nc<grid[0].length&&
-                    grid[nr][nc]=='1'){
-                        q.add(new int[]{nr,nc});
-                        grid[nr][nc]=0;
-                    }
+                if(nr>=0&&nc>=0&&nr<grid.length&&nc<grid[0].length&&grid[nr][nc]=='1'){
+                    q.add(new int[]{nr,nc});
+                    grid[nr][nc]='0';
+                }
             }
         }
         return;
     }
-    private int[][] dir=new int[][]{
+    private int[][] dirs= new int[][]{
         {1,0},
         {-1,0},
         {0,1},
