9302ff0637b922142dfc527ddcc02856df5f4604
Yushi Cui <qs8cys@gmail.com>
2025-05-15 08:14:02 +1200

    Time: 5 ms (33.03%) | Memory: 48.8 MB (95.21%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,40 +1,43 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int ROWS=grid.length;
-        int COLS=grid[0].length;
+        int COLS= grid[0].length;
         int res=0;
-        for(int r=0;r<ROWS;r++){
-            for(int c=0; c<COLS; c++){
+        for(int r=0; r<ROWS;r++){
+            for(int c=0;c<COLS;c++){
                 if(grid[r][c]=='1'){
-                    bfs(grid,r,c);
                     res++;
+                    bfs(grid,r,c);
                 }
             }
         }
         return res;
     }
     private void bfs(char[][] grid, int r, int c){
-        Queue<int[]> q= new LinkedList<>();
+        Queue<int[]> q=new LinkedList<>();
         q.add(new int[]{r,c});
+        grid[r][c]='0';
         while(!q.isEmpty()){
-            int[] p= q.remove();
-            int row=p[0];
-            int col=p[1];
-            for(int[] d:dirs){
-                int nr=row+d[0];
-                int nc=col+d[1];
-                if(nr>=0&&nc>=0&&nr<grid.length&&nc<grid[0].length&&grid[nr][nc]=='1'){
-                    q.add(new int[]{nr,nc});
-                    grid[nr][nc]='0';
+            int[] p=q.remove();
+            for(int d[]:dirs){
+                int nr=p[0]+d[0];
+                int nc=p[1]+d[1];
+                if(nr>=0&&nc>=0){
+                    if(nr<grid.length&&nc<grid[0].length){
+                        if(grid[nr][nc]=='1'){
+                            q.add(new int[]{nr,nc});
+                            grid[nr][nc]='0';
+                        }
+                    }
                 }
             }
         }
         return;
     }
-    private int[][] dirs= new int[][]{
+    private int[][] dirs=new int[][]{
         {1,0},
         {-1,0},
         {0,1},
-        {0,-1}
+        {0,-1},
     };
 }
