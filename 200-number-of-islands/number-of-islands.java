45052bbacf0b0abbabe2b6b4985a403290c8bf9d
Yushi Cui <qs8cys@gmail.com>
2025-05-14 19:55:01 +1200

    Time: 5 ms (33.01%) | Memory: 52.3 MB (17.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -3,41 +3,41 @@ class Solution {
         int ROWS=grid.length;
         int COLS=grid[0].length;
         int res=0;
-        for(int r=0;r<ROWS;r++){
+        for(int r=0; r<ROWS;r++){
             for(int c=0; c<COLS; c++){
                 if(grid[r][c]=='1'){
-                    bsf(grid,r,c);
-                    res++;
+
+                bfs(grid,r,c);
+                res++;
                 }
             }
         }
         return res;
     }
-    private int[][] dirs={
+    private void bfs(char [][] grid, int r, int c){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{r,c});
+        while(!q.isEmpty()){
+            int[] p=q.remove();
+            int row=p[0];
+            int col=p[1];
+            for(int[] d:dir){
+                int nr=row+d[0];
+                int nc=col+d[1];
+                if(nr>=0&&nc>=0&&
+                    nr<grid.length&&nc<grid[0].length&&
+                    grid[nr][nc]=='1'){
+                        q.add(new int[]{nr,nc});
+                        grid[nr][nc]=0;
+                    }
+            }
+        }
+        return;
+    }
+    private int[][] dir=new int[][]{
         {1,0},
         {-1,0},
         {0,1},
         {0,-1}
     };
-    private void bsf(char[][] grid,int r, int c){
-        Queue<int[]> q= new LinkedList<>();
-        grid[r][c]='0';
-        q.add(new int[]{r,c});
-        while(!q.isEmpty()){
-            int[] node =q.remove();
-            int row=node[0];
-            int col=node[1];
-            for(int[] dir:this.dirs){
-                int nr=row+dir[0];
-                int nc=col+dir[1];
-                if(nr<grid.length&&nc<grid[0].length&&
-                nr>=0 && nc>=0 &&grid[nr][nc]=='1'){
-                    q.add(new int[]{nr,nc});
-                    grid[nr][nc]='0';
-                }
-            }
-
-            
-        }
-    }
 }
