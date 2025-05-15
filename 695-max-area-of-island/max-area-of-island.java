d033c7294c269b000b9ed0f4be26ab2ab728069b
Yushi Cui <qs8cys@gmail.com>
2025-05-15 14:39:46 +1200

    Time: 2 ms (70.28%) | Memory: 44.6 MB (59.61%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.java
+++ 695-max-area-of-island/max-area-of-island.java
@@ -1,41 +1,33 @@
 class Solution {
     public int maxAreaOfIsland(int[][] grid) {
-        int ROWS=grid.length;
+        int ROWS= grid.length;
         int COLS= grid[0].length;
-        Queue<Integer> q= new PriorityQueue<>(Collections.reverseOrder());
-        for(int r=0; r<ROWS; r++){
-            for(int c=0; c<COLS; c++){
+        int area=0;
+        for(int r=0; r<ROWS;r++){
+            for(int c=0; c<COLS;c++){
                 if(grid[r][c]==1){
-                    q.add(bfs(grid,r,c));
-                }
-            }
-        }
-        q.add(0);
-        return q.peek();
-    }
-    public int bfs(int[][] grid, int r, int c){
-        Queue<int[]> q=new LinkedList<>();
-        int area=1;
-        q.add(new int[]{r,c});
-        grid[r][c]=0;
-        while(!q.isEmpty()){
-            int[] p= q.remove();
-            for(int[] d: dirs){
-                int nr= p[0]+d[0];
-                int nc= p[1]+d[1];
-                if(nr>=0&&nc>=0 && nr<grid.length&&nc<grid[0].length && grid[nr][nc]==1){
-                    q.add(new int[]{nr,nc});
-                    grid[nr][nc]= 0;
-                    area++;
+                    area=Math.max(area,dfs(grid,r,c));
                 }
             }
         }
         return area;
     }
+    private int dfs(int[][] grid, int r, int c){
+        if(r<0||c<0 ||r>=grid.length|| c>=grid[0].length || grid[r][c]==0){
+            return 0;
+        }
+        grid[r][c]=0;
+        int res=1;
+        for(int[] d:dirs){
+            res+=dfs(grid, r+d[0], c+d[1]);
+        }
+        return res;
+
+    }
     private int[][] dirs= new int[][]{
         {1,0},
         {-1,0},
         {0,1},
-        {0,-1}
+        {0,-1},
     };
 }
