4759c7cf2374d7a388eb5af3e4f55ae74499415b
Yushi Cui <qs8cys@gmail.com>
2025-05-17 09:24:41 +1200

    Time: 25 ms (42.86%) | Memory: 45.6 MB (9.64%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,28 +1,29 @@
 class Solution {
-    public int eating(int[] piles, int speed){
+    private int eating(int[] piles, int speed){
         int res=0;
-        for(int b:piles){
-            res+=Math.ceil((double)b/speed);
+        for(int p:piles){
+            res+=Math.ceil((double)p/speed);
         }
         return res;
     }
     public int minEatingSpeed(int[] piles, int h) {
         int l=1;
         int r=0;
-        for(int b:piles){
-            if(r<b){
-                r=b;
-            }
+        int min=0;
+        for(int n: piles){
+            if(r<n){r=n;};
         }
         while(l<=r){
-            int m=l+(r-l)/2;
-            int th=eating(piles,m);
-            if(th>h){
+            int m= l+(r-l)/2;
+            int time=eating(piles,m);
+            if(h<time){
                 l=m+1;
             }else{
                 r=m-1;
+                min=m;
             }
         }
-        return l;
+        return  min;
+        
     }
 }
