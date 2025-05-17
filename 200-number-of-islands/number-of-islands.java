854c6f40616618539cfd394617ba97f6a30a2c43
Yushi Cui <qs8cys@gmail.com>
2025-05-18 10:20:59 +1200

    Time: 3 ms (84.18%) | Memory: 49.9 MB (39.11%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,29 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
         int ROWS=grid.length;
-        int COLS=grid[0].length;
-        int number=0;
-        for(int r=0; r<ROWS; r++){
-            for(int c=0;c<COLS;c++){
+        int COLS= grid[0].length;
+        int i=0;
+        for(int r=0;r<ROWS;r++){
+            for(int c=0; c<COLS;c++){
                 if(grid[r][c]=='1'){
                     dfs(grid,r,c);
-                    number++;
+                    i++;
                 }
             }
         }
-        return number;
+        return i;
     }
-    private void dfs(char[][] grid, int r, int c){
-        if(r<0||c<0||r>=grid.length||c >=grid[0].length||grid[r][c]=='0'){
-            return;
+    private void dfs(char[][] grid,int r, int c){
+        if(r<0||c<0 || r>=grid.length||c>=grid[0].length || grid[r][c]=='0'){
+            return; 
         }
-        grid[r][c]='0';
-        for(int[] d:dir){
+        for(int[] d:dirs){
+            grid[r][c]='0';
             dfs(grid,r+d[0],c+d[1]);
         }
         return;
     }
-    private int[][] dir= new int[][]{
-        {0,1},{0,-1},{1,0},{-1,0}
+    private int[][] dirs=new int[][]{
+        {0,1},
+        {0,-1},
+        {1,0},
+        {-1,0},
     };
 }
