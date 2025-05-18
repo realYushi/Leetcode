b176b816891035731b99ac9e5c179a8de2e62c04
Yushi Cui <qs8cys@gmail.com>
2025-05-18 12:17:37 +1200

    Time: 13 ms (83.97%) | Memory: 46 MB (16.77%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,7 +1,7 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int n=grid.length;
-        if(grid[0][0]==1||grid[n-1][n-1]==1){
+        int n= grid.length;
+        if(grid[0][0]==1 || grid[n-1][n-1]==1){
             return -1;
         }
         if(n==1){
@@ -9,25 +9,24 @@ class Solution {
         }
         return bfs(grid,0,0);
     }
-    private int bfs(int[][] grid,int r, int c){
-        int n= grid.length;
-        Queue<int[]> q= new LinkedList<>();
+    private int bfs(int[][] grid, int r, int c){
+        int p=1;
+        Queue<int[]>q=new LinkedList<>();
         q.add(new int[]{0,0});
         grid[0][0]=1;
-        int p=1;
         while(!q.isEmpty()){
-            int len =q.size();
+            int len=q.size();
             for(int i=0; i<len; i++){
                 int[] cell=q.remove();
-                if(cell[0]==n-1&&cell[1]==n-1){
+                if(cell[0]==grid.length-1&&cell[1]==grid[0].length-1){
                     return p;
                 }
                 for(int[] d:dirs){
                     int nr=cell[0]+d[0];
                     int nc=cell[1]+d[1];
-                    if(nr>=0&&nc>=0 && nr<n&&nc<n && grid[nr][nc]==0){
-                        grid[nr][nc]=1;
+                    if(nr>=0&&nc>=0&&nr<grid.length&&nc<grid[0].length&&grid[nr][nc]==0){
                         q.add(new int[]{nr,nc});
+                        grid[nr][nc]=1;
                     }
                 }
             }
@@ -36,9 +35,7 @@ class Solution {
         return -1;
     }
     private int[][] dirs= new int[][]{
-        {0,1},{0,-1},{1,0},{-1,0},
-        {1,1},{1,-1},{-1,1},{-1,-1},
+        {1,0},{-1,0},{0,1},{0,-1},
+        {1,1},{-1,1},{1,-1},{-1,-1},
     };
-
-    
 }
