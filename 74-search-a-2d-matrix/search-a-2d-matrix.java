d0675fb7d37b651d4fd57a5b59bb880acae19174
Yushi Cui <qs8cys@gmail.com>
2025-05-20 07:38:47 +1200

    Time: 0 ms (100.00%) | Memory: 42.5 MB (18.40%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,21 +1,21 @@
 class Solution {
-    public int bs1(int[][] matrix,int target){
+    private int bs1(int[][] matrix, int target){
         int l=0;
         int r=matrix.length-1;
-        int rowL=matrix[0].length-1;
+        int rowLen=matrix[0].length-1;
         while(l<=r){
             int m= l+(r-l)/2;
             if(target<matrix[m][0]){
                 r=m-1;
-            }else if(target>matrix[m][rowL]){
+            }else if(target>matrix[m][rowLen]){
                 l=m+1;
-            }else{
+            }else {
                 return m;
             }
         }
-        return  -1;
+        return -1;
     }
-    public boolean bs2(int[] list, int target){
+    private int bs2(int[]list, int target){
         int l=0;
         int r=list.length-1;
         while(l<=r){
@@ -25,17 +25,17 @@ class Solution {
             }else if(target>list[m]){
                 l=m+1;
             }else{
-                return true;
+                return m;
             }
         }
-        return false;
+        return -1;
     }
     public boolean searchMatrix(int[][] matrix, int target) {
         int row=bs1(matrix,target);
         if(row==-1){
             return false;
         }
-        return bs2(matrix[row],target);
+        return bs2(matrix[row],target)>=0;
         
     }
 }
