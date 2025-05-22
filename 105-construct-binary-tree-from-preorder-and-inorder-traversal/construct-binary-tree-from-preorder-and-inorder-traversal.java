54db020b89b67c6f809896b3f5b452abc1731741
Yushi Cui <qs8cys@gmail.com>
2025-05-22 12:50:15 +1200

    Time: 11 ms (5.04%) | Memory: 84 MB (5.00%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -18,15 +18,13 @@ class Solution {
         if(preorder.length==0||inorder.length==0){
             return null;
         }
-        TreeNode root=new TreeNode(preorder[0]);
+        TreeNode root= new TreeNode(preorder[0]);
         int m=-1;
-        for(int i=0; i<inorder.length;i++){
+        for(int i=0; i<inorder.length; i++){
             if(root.val==inorder[i]){
                 m=i;
-                break;
             }
         }
-
         int[] lpreorder=Arrays.copyOfRange(preorder,1,m+1);
         int[] linorder=Arrays.copyOfRange(inorder,0,m);
         root.left=buildTree(lpreorder,linorder);
@@ -36,5 +34,6 @@ class Solution {
         root.right=buildTree(rpreorder,rinorder);
 
         return root;
+        
     }
 }
