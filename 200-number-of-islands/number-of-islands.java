8453406ab87df7d0aa483ae9080066c585c05279
Yushi Cui <qs8cys@gmail.com>
2025-05-23 09:28:10 +1200

    Time: 3 ms (84.24%) | Memory: 49.8 MB (39.48%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,27 +1,32 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int res=0;
-        for(int r= 0; r<grid.length;r++){
+        int num= 0;
+        for(int r=0; r<grid.length;r++){
             for(int c=0; c<grid[0].length;c++){
                 if(grid[r][c]=='1'){
-                    dfs(grid,r,c);
-                    res++;
+                    num++;
+                    bfs(grid,r,c);
                 }
             }
         }
-        return res;
+        return num;
     }
-    private void dfs(char[][] grid,int r, int c){
-        if(r<0||c<0||r>=grid.length||c>=grid[0].length ||grid[r][c]=='0'){
-            return;
+    private void bfs(char[][] grid, int r, int c){
+        if(r<0||c<0||r>=grid.length||c>=grid[0].length||grid[r][c]=='0'){
+            return ;
         }
         grid[r][c]='0';
-        for(int d[]:dirs){
-            dfs(grid,r+d[0],c+d[1]);
+        for(int[] d:dirs){
+            int nr=r+d[0];
+            int nc=c+d[1];
+            bfs(grid,nr,nc);
         }
         return;
     }
-    private int[][] dirs=new int[][]{
-        {0,1},{0,-1},{1,0},{-1,0},
+    int[][] dirs =new int[][]{
+        {0,1},
+        {0,-1},
+        {1,0},
+        {-1,0},
     };
 }
