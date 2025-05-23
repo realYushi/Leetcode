ffb94d5dedd0d47da90f283370ac414300486f34
Yushi Cui <qs8cys@gmail.com>
2025-05-24 10:26:06 +1200

    Time: 2 ms (83.41%) | Memory: 44.6 MB (81.20%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,23 +1,21 @@
 class Solution {
-    public void dfs(List<List<Integer>> res, List<Integer> sub, int[] candidates,int i,int target){
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> list= new ArrayList<>();
+        List<Integer> sub= new ArrayList<>();
+        dfs(candidates,list,sub,target,0);
+        return list;
+    }
+    private void dfs(int[] candidates, List<List<Integer>>list, List<Integer>sub, int target, int index ){
         if(target==0){
-            res.add(new ArrayList<>(sub));
-            return; 
-        }
-        if(i>=candidates.length||target<0){
+            list.add(new ArrayList<>(sub));
             return;
         }
-        sub.add(candidates[i]);
-        dfs(res,sub,candidates,i,target-candidates[i]);
-
+        if(index>=candidates.length||target<0){
+            return;
+        }
+        sub.add(candidates[index]);
+        dfs(candidates,list,sub,target-candidates[index],index);
         sub.remove(sub.size()-1);
-        dfs(res,sub,candidates,i+1,target);
-    }
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res=new ArrayList<>();
-        List<Integer> sub= new ArrayList<>();
-        dfs(res,sub,candidates,0,target);
-        return res;
-        
+        dfs(candidates,list,sub,target,index+1);
     }
 }
