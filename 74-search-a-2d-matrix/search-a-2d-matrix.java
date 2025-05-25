692d7b325f67e05db67fd4264a5f33c3e92e7e96
Yushi Cui <qs8cys@gmail.com>
2025-05-26 11:37:55 +1200

    Time: 0 ms (100.00%) | Memory: 41.8 MB (96.81%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,26 +1,12 @@
 class Solution {
-    private int bs2(int[] list, int target){
-        int l=0;
-        int r=list.length-1;
-        while(l<=r){
-            int m= l+(r-l)/2;
-            if(target < list[m]){
-                r=m-1;
-            }else if(target>list[m]){
-                l=m+1;
-            }else{
-                return m;
-            }
-        }
-        return -1;
-    }
     private int bs1(int[][] matrix, int target){
+        int res=0;
         int l=0;
         int r=matrix.length-1;
-        int rowLen= matrix[0].length-1;
+        int rowLen=matrix[0].length-1;
         while(l<=r){
-            int m= l+(r-l)/2;
-            if(target < matrix[m][0]){
+            int m=l+(r-l)/2;
+            if(target<matrix[m][0]){
                 r=m-1;
             }else if(target>matrix[m][rowLen]){
                 l=m+1;
@@ -30,20 +16,31 @@ class Solution {
         }
         return -1;
     }
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int row=bs1(matrix,target);
-        if(row==-1){
-            return false;
-        }else{
-            int index= bs2(matrix[row],target);
-            if(index==-1){
-                return false;
+    private int bs2(int[] list, int target){
+        int res=0;
+        int l=0;
+        int r=list.length-1;
+        while(l<=r){
+            int m= l+(r-l)/2;
+            if(target<list[m]){
+                r=m-1;
+            }else if(target>list[m]){
+                l=m+1;
+            }else{
+                return m;
             }
         }
-        return true;
+        return  -1;
 
+    }
 
-
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row= bs1(matrix,target);
+        if(row==-1){
+            return false;
+        }
+        return bs2(matrix[row],target)!=-1;
+        
         
     }
 }
