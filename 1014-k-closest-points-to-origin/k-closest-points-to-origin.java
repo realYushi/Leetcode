8dfd538335634efed39614bee1054872766a4287
Yushi Cui <qs8cys@gmail.com>
2025-05-31 14:30:57 +1200

    Time: 70 ms (9.09%) | Memory: 55.5 MB (19.44%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,16 +1,21 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Queue<int[]> q= new PriorityQueue<>(Comparator.comparing((a)->a[0]));
-        for(int[] p:points){
-            q.add(new int[]{p[0]*p[0]+p[1]*p[1],p[0],p[1]});
+        Queue<int[]> q=new PriorityQueue<>(Comparator.comparing(a->-a[0]));
+        for(int[]p:points){
+            int d=p[0]*p[0]+p[1]*p[1];
+            q.add(new int[]{d,p[0],p[1]});
+            if(q.size()>k){
+                q.remove();
+            }
         }
-        int[][] res = new int[k][2];
-        for(int i=0; i<k;i++){
-            int[] t= q.remove();
-            res[i][0]=t[1];
-            res[i][1]=t[2];
+        
+        int[][] res= new int[k][2];
+        int len=q.size();
+        for(int i=0; i<len; i++){
+            int[] p=q.remove();
+            res[i][0]=p[1];
+            res[i][1]=p[2];
         }
         return res;
-        
     }
 }
