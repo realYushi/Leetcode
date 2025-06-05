3227f10eca24bdcc276db96f363200c11e701657
Yushi Cui <qs8cys@gmail.com>
2025-06-05 19:43:42 +1200

    Time: 0 ms (100.00%) | Memory: 42.2 MB (66.38%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,46 +1,42 @@
 class Solution {
-    private int bs1(int[][] matrix, int target){
-        int res=0;
+
+    public int bs1(int[][] matix, int target){
         int l=0;
-        int r=matrix.length-1;
-        int rowLen=matrix[0].length-1;
+        int r=matix.length-1;
+        int row=matix[0].length-1;
         while(l<=r){
             int m=l+(r-l)/2;
-            if(target<matrix[m][0]){
+            if(target<matix[m][0]){
                 r=m-1;
-            }else if(target>matrix[m][rowLen]){
-                l=m+1;
-            }else{
-                return m;
-            }
-        }
-        return -1;
-    }
-    private int bs2(int[] list, int target){
-        int res=0;
-        int l=0;
-        int r=list.length-1;
-        while(l<=r){
-            int m= l+(r-l)/2;
-            if(target<list[m]){
-                r=m-1;
-            }else if(target>list[m]){
+            }else if(target >matix[m][row]){
                 l=m+1;
             }else{
                 return m;
             }
         }
         return  -1;
-
     }
-
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int row= bs1(matrix,target);
-        if(row==-1){
+    public boolean bs2(int[][] matrix, int rowNumber, int target){
+        if(rowNumber==-1){
             return false;
         }
-        return bs2(matrix[row],target)!=-1;
-        
-        
+        int[] row= matrix[rowNumber];
+        int l=0;
+        int r=matrix[0].length-1;
+        while(l<=r){
+            int m=l+(r-l)/2;
+            if(target<row[m]){
+                r=m-1;
+            }else if(target>row[m]){
+                l=m+1;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int rowNumber=bs1(matrix,target);
+        return bs2(matrix,rowNumber,target);
     }
 }
