7846923d6b40451abc30300798327d1b7bee42a7
Yushi Cui <qs8cys@gmail.com>
2025-06-06 12:21:35 +1200

    Time: 48 ms (47.97%) | Memory: 116.4 MB (33.58%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,28 +1,31 @@
 class Node{
-    public Node prev;
-    public Node next;
-    public int val;
-    public int key;
+    int val;
+    int key;
+    Node prev;
+    Node next;
     public Node(int key, int val){
         this.key=key;
         this.val=val;
-        this.prev=null;
-        this.next=null;
+        Node prev=null;
+        Node next=null;
     }
     public Node(){
         this.key=0;
         this.val=0;
-        this.prev=null;
-        this.next=null;
+        Node prev=null;
+        Node next=null;
     }
 }
 class LRUCache {
+    int cap;
+    List<Node> list;
     Node dummyHead;
     Node dummyTail;
-    Map<Integer,Node> cache;
-    int cap;
+    Map <Integer, Node> cache;
+
     public LRUCache(int capacity) {
         this.cap=capacity;
+        this.list=new LinkedList<>();
         this.cache=new HashMap<>();
         this.dummyHead=new Node();
         this.dummyTail=new Node();
@@ -32,8 +35,8 @@ class LRUCache {
     private void insert(Node node){
         node.next=this.dummyTail;
         node.prev=this.dummyTail.prev;
-        node.next.prev=node;
         node.prev.next=node;
+        node.next.prev=node;
         return;
     }
     private void remove(Node node){
@@ -49,22 +52,27 @@ class LRUCache {
             insert(node);
             return node.val;
         }
-        return  -1;
+        return -1;
     }
     
+
     public void put(int key, int value) {
-        if(this.cache.containsKey(key)){
-            remove(this.cache.get(key));
-        }
         Node node = new Node(key,value);
-        this.cache.put(key,node);
-        insert(node);
-        if(this.cap<this.cache.size()){
-            Node lru= this.dummyHead.next;
-            remove(lru);
-            this.cache.remove(lru.key);
+        if(this.cache.containsKey(node.key)){
+            Node temp=this.cache.get(node.key);
+            this.cache.put(node.key,node);
+            remove(temp);
+            insert(node);
+        }else{
+            this.cache.put(node.key,node);
+            insert(node);
         }
-        return;
+        if(this.cache.size()>this.cap){
+            Node lru= this.dummyHead.next;
+            this.cache.remove(lru.key);
+            remove(lru);
+
+        }
         
     }
 }
