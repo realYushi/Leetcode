7b18c7cb4206cdd2be07b191f5c5daaf1c250c3d
Yushi Cui <qs8cys@gmail.com>
2025-06-06 14:44:46 +1200

    Time: 1 ms (100.00%) | Memory: 42.6 MB (39.37%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,10 +1,11 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
         int fresh=0;
-        int turn=0;
-        Queue<int[]> q=new LinkedList<>();
-        for(int r=0;r<grid.length;r++){
-            for(int c=0; c<grid[0].length;c++){
+        Queue<int[]> q= new LinkedList<>();
+        int res=0;
+
+        for(int r=0; r<grid.length;r++){
+            for(int c=0; c<grid[0].length; c++){
                 if(grid[r][c]==1){
                     fresh++;
                 }
@@ -16,24 +17,25 @@ class Solution {
         int[][] dirs=new int[][]{
             {0,1},{0,-1},{1,0},{-1,0}
         };
+
         while(fresh>0&&!q.isEmpty()){
             int len=q.size();
-            for(int i=0; i<len;i++){
-                int[]p=q.remove();
+            for(int i=0; i<len; i++){
+                int[] p=q.remove();
                 for(int[] d:dirs){
                     int nr=p[0]+d[0];
                     int nc=p[1]+d[1];
                     if(nr>=0&&nc>=0&&nr<grid.length&&nc<grid[0].length&&grid[nr][nc]==1){
-                        fresh--;
                         grid[nr][nc]=2;
                         q.add(new int[]{nr,nc});
+                        fresh--;
                     }
                 }
             }
-
-            turn++;
+            res++;
         }
-        return fresh==0?turn:-1;
+        return fresh==0?res:-1;
+
         
     }
 }
