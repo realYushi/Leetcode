4bda550c461751648173900d61f7dae25f7164ef
Yushi Cui <qs8cys@gmail.com>
2025-06-09 22:13:24 +1200

    Time: 3 ms (84.20%) | Memory: 49.5 MB (61.09%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,7 +2,7 @@ class Solution {
     public int numIslands(char[][] grid) {
         int res=0;
         for(int r=0; r<grid.length; r++){
-            for(int c=0; c<grid[0].length; c++){
+            for(int c=0; c<grid[0].length;c++){
                 if(grid[r][c]=='1'){
                     res++;
                     dfs(grid,r,c);
@@ -11,17 +11,17 @@ class Solution {
         }
         return res;
     }
-    private void dfs(char[][] grid,int r, int c){
-        if(r<0||c<0||r>=grid.length||c>=grid[0].length||grid[r][c]=='0'){
+    private void dfs(char[][] gird, int r, int c){
+        if(r<0||c<0||r>=gird.length||c>=gird[0].length||gird[r][c]=='0'){
             return;
         }
-        grid[r][c]='0';
+        gird[r][c]='0';
         for(int[] d:dirs){
-            dfs(grid,r+d[0],c+d[1]);
+            dfs(gird,r+d[0],c+d[1]);
         }
         return;
     }
-    private int[][] dirs=new int[][]{
-        {1,0},{-1,0},{0,1},{0,-1}
+    private int[][] dirs= new int[][]{
+        {0,1},{0,-1},{1,0},{-1,0}
     };
 }
