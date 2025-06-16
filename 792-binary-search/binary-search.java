821517e216a8f6d0d6729f427737c739018cf696
Yushi Cui <qs8cys@gmail.com>
2025-06-17 11:24:50 +1200

    Time: 0 ms (100.00%) | Memory: 46.1 MB (14.14%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,10 +4,10 @@ class Solution {
         int r=nums.length-1;
         while(l<=r){
             int m=l+(r-l)/2;
-            if(target>nums[m]){
-                l=m+1;
-            }else if(target<nums[m]){
+            if(target<nums[m]){
                 r=m-1;
+            }else if(target>nums[m]){
+                l=m+1;
             }else{
                 return m;
             }
