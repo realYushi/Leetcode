2bbd94af91aa2d8a6c9dbc0414d7c1bde932599d
Yushi Cui <qs8cys@gmail.com>
2025-06-17 17:24:22 +1200

    Time: 0 ms (100.00%) | Memory: 40.4 MB (65.96%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,16 +1,22 @@
-public class Solution {
+class Solution {
     int[] cache;
     public int climbStairs(int n) {
-        cache = new int[n];
-        for (int i = 0; i < n; i++) {
-            cache[i] = -1;
+        this.cache=new int[n+1];
+        for(int i=0; i<=n; i++){
+            cache[i]=-1;
         }
-        return dfs(n, 0); 
+        return dfs(n);
     }
-
-    public int dfs(int n, int i) {
-        if (i >= n) return i == n ? 1 : 0;
-        if (cache[i] != -1) return cache[i];
-        return cache[i] = dfs(n, i + 1) + dfs(n, i + 2);
+    private int dfs(int n){
+        if(n==1){
+            return 1;
+        }
+        if(n==2){
+            return 2;
+        }
+        if(cache[n]!=-1){
+            return cache[n];
+        }
+        return cache[n]=dfs(n-1)+dfs(n-2);
     }
 }
