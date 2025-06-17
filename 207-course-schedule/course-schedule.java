efd9e36d0e8c885b152d8a5d26f6a9ae2bf81691
Yushi Cui <qs8cys@gmail.com>
2025-06-17 12:06:42 +1200

    Time: 7 ms (47.86%) | Memory: 46.4 MB (7.02%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,37 +1,36 @@
 class Solution {
-    Map<Integer,List<Integer>> pre=new HashMap<>();
-    Set<Integer> visited= new HashSet<>();
+    Map<Integer,List<Integer>> map= new HashMap<>();
+    Set<Integer> set= new HashSet<>();
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         for(int i=0; i<numCourses; i++){
-            pre.put(i,new ArrayList<>());
+            map.put(i,new ArrayList<>());
         }
-        for(int[] pc:prerequisites){
-            pre.get(pc[0]).add(pc[1]);
+        for(int[] pre:prerequisites){
+            map.get(pre[0]).add(pre[1]);
         }
-        for(int i=0; i<numCourses;i++){
+        for(int i=0; i<numCourses; i++){
             if(!dfs(i)){
                 return false;
             }
         }
         return true;
-
+        
     }
     private boolean dfs(int course){
-        if(visited.contains(course)){
+        if(set.contains(course)){
             return false;
         }
-        if(pre.get(course).isEmpty()){
+        if(map.get(course).isEmpty()){
             return true;
         }
-        visited.add(course);
-        for(int c:pre.get(course)){
-            if(!dfs(c)){
+        set.add(course);
+        for(int pr:map.get(course)){
+            if(!dfs(pr)){
                 return false;
             }
         }
-        visited.remove(course);
-        pre.put(course,new ArrayList<>());
+        set.remove(course);
+        map.put(course,new ArrayList<>());
         return true;
-
     }
 }
