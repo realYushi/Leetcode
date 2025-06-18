5074408af01984ca191f89760c75c19006a6fed6
Yushi Cui <qs8cys@gmail.com>
2025-06-18 22:55:34 +1200

    Time: 0 ms (100.00%) | Memory: 41.1 MB (60.54%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,18 +1,18 @@
 class Solution {
-    int[] cache;
     public int rob(int[] nums) {
-        cache=new int[nums.length];
-        Arrays.fill(cache,-1);
-        return dfs(nums,0);
-    }
-    private int dfs(int[] nums, int i){
-        if(i>=nums.length){
+        if(nums.length==0){
             return 0;
         }
-        if(cache[i]!=-1){
-            return cache[i];
+        if(nums.length==1){
+            return nums[0];
         }
-        cache[i]=Math.max(nums[i]+dfs(nums,i+2),dfs(nums,i+1));
-        return cache[i];
+        int[] cache = new int[nums.length];
+        cache[0]=nums[0];
+        cache[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<nums.length;i++){
+            cache[i]=Math.max(nums[i]+cache[i-2], cache[i-1]);
+        }
+        return cache[nums.length-1];
+        
     }
 }
