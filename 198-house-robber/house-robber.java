1012b85b7465217323195b4abe192b59283d0aeb
Yushi Cui <qs8cys@gmail.com>
2025-06-18 23:07:40 +1200

    Time: 0 ms (100.00%) | Memory: 41.4 MB (15.41%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -9,8 +9,8 @@ class Solution {
         int[] dp= new int[nums.length];
         dp[0]=nums[0];
         dp[1]=Math.max(nums[0],nums[1]);
-        for(int i=2; i<nums.length;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        for(int i=2; i<nums.length; i++){
+            dp[i]=Math.max(nums[i]+dp[i-2], dp[i-1]);
         }
         return dp[nums.length-1];
         
