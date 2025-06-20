e8820565d7fd36e6b1bad7050e5b229756dcab47
Yushi Cui <qs8cys@gmail.com>
2025-06-20 12:43:05 +1200

    Time: 9 ms (58.09%) | Memory: 45.9 MB (28.43%) - LeetSync

--- 838-design-linked-list/design-linked-list.java
+++ 838-design-linked-list/design-linked-list.java
@@ -1,36 +1,29 @@
-class Node{
+class Node {
     int val;
     Node next;
-    Node prev;
     public Node(int val){
         this.val=val;
-        this.next=null;
-        this.prev=null;
+        this.next= null;
     }
     public Node(){
-        this.val=0;
+        this.val=-1;
         this.next=null;
-        this.prev=null;
     }
 }
 class MyLinkedList {
     Node dummyHead;
-    Node dummyTail;
     int size;
 
     public MyLinkedList() {
         this.dummyHead=new Node();
-        this.dummyTail=new Node();
-        this.dummyHead.next=this.dummyTail;
-        this.dummyTail.prev=this.dummyHead;
         this.size=0;
     }
     
     public int get(int index) {
-        if(index>=this.size){
+        if(index<0||index>=size){
             return -1;
         }
-        Node cur=this.dummyHead.next;
+        Node cur= dummyHead.next;
         for(int i=0; i<index;i++){
             cur=cur.next;
         }
@@ -38,62 +31,57 @@ class MyLinkedList {
     }
     
     public void addAtHead(int val) {
-        Node newHead=new Node(val);
-        newHead.prev=this.dummyHead;
-        newHead.next=this.dummyHead.next;
-        newHead.prev.next=newHead;
-        newHead.next.prev=newHead;
-        this.size++;
+        Node node=new Node(val);
+        node.next=dummyHead.next;
+        dummyHead.next=node;
+        size++;
+        return;
     }
     
     public void addAtTail(int val) {
-        Node newTail=new Node(val);
-        newTail.next=this.dummyTail;
-        newTail.prev=this.dummyTail.prev;
-        newTail.next.prev=newTail;
-        newTail.prev.next=newTail;
-        this.size++;
+        Node node = new Node(val);
+        Node cur= dummyHead;
+        while(cur.next!=null){
+            cur=cur.next;
+        }
+        cur.next=node;
+        size++;
+        return;
     }
     
     public void addAtIndex(int index, int val) {
-        if(index>this.size){
-            return;
-        }
-        if(index==this.size){
-            this.addAtTail(val);
+        if(index<0||index>size){
             return;
         }
         if(index==0){
-            this.addAtHead(val);
+            addAtHead(val);
+            return;
+        }
+        if(index==size){
+            addAtTail(val);
             return;
         }
         Node node = new Node(val);
-        Node cur= this.dummyHead.next;
-        for(int i= 0; i<index; i++){
+        Node cur= dummyHead;
+        for(int i=0; i<index; i++){
             cur=cur.next;
         }
-        node.next=cur;
-        node.prev=cur.prev;
-        node.next.prev=node;
-        node.prev.next=node;
-        this.size++;
+        node.next=cur.next;
+        cur.next=node;
+        size++;
         return;
-        
     }
     
     public void deleteAtIndex(int index) {
-        if(index>=this.size){
+        if(index<0||index>=size){
             return;
         }
-        Node node= this.dummyHead.next;
+        Node cur=dummyHead;
         for(int i=0; i<index;i++){
-            node=node.next;
+            cur=cur.next;
         }
-        Node prev=node.prev;
-        Node next=node.next;
-        prev.next=next;
-        next.prev=prev;
-        this.size--;
+        cur.next=cur.next.next;
+        size--;
         return;
         
     }
