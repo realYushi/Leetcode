e431ab4739448914b728ea9584d8c43f4cc8a533
Yushi Cui <qs8cys@gmail.com>
2025-06-27 17:15:14 +1200

    Time: 13 ms (85.67%) | Memory: 45.6 MB (65.65%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.java
@@ -1,30 +1,30 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
-        int ROWS=grid.length;
-        int COLS=grid[0].length;
-        if(grid[0][0]==1||grid[ROWS-1][COLS-1]==1){
+        int row = grid.length - 1;
+        int col = grid[0].length - 1;
+        if (grid[0][0] == 1 || grid[row][col] == 1) {
             return -1;
         }
-        int res=1;
-        Queue<int[]> q= new LinkedList<>();
-        q.add(new int[]{0,0});
-        int[][] dirs=new int[][]{
-            {0,1},{0,-1},{1,0},{-1,0},
-            {1,1},{-1,-1},{1,-1},{-1,1},
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] { 0, 0 });
+        int res = 1;
+        int[][] dirs = new int[][] {
+                { 0, 1 }, { 1, 0 }, { 0, -1 },{-1,0},
+                { 1, 1 }, { -1, -1 }, { 1, -1 },{-1,1},
         };
-        while(!q.isEmpty()){
-            int len=q.size();
-            for(int i=0; i<len; i++){
-                int[] p=q.remove();
-                if(p[0]==ROWS-1 && p[1]==COLS-1){
+        while (!q.isEmpty()) {
+            int len = q.size();
+            for (int i = 0; i < len; i++) {
+                int[] p = q.remove();
+                if (p[0] == row && p[1] == col) {
                     return res;
                 }
-                for(int[] d:dirs){
-                    int nr=p[0]+d[0];
-                    int nc=p[1]+d[1];
-                    if(nr>=0&&nc>=0&&nr<ROWS&&nc<COLS&&grid[nr][nc]==0){
-                        q.add(new int[]{nr,nc});
-                        grid[nr][nc]=1;
+                for (int[] d : dirs) {
+                    int nr = p[0] + d[0];
+                    int nc = p[1] + d[1];
+                    if (nr >= 0 && nc >= 0 && nr < grid.length && nc < grid[0].length && grid[nr][nc] == 0) {
+                        grid[nr][nc] = 1;
+                        q.add(new int[] { nr, nc });
                     }
                 }
             }
@@ -32,6 +32,5 @@ class Solution {
         }
         return -1;
 
-        
     }
 }
