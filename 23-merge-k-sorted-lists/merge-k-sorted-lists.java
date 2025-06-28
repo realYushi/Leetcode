a8365a0c9df3997c27e8df69c5918067b92f5449
Yushi Cui <qs8cys@gmail.com>
2025-06-29 09:29:54 +1200

    Time: 80 ms (15.68%) | Memory: 44.8 MB (20.39%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,35 +9,32 @@
  * }
  */
 class Solution {
-    public ListNode mergeLists(ListNode list1, ListNode list2){
+    private ListNode merge2Lists(ListNode list1, ListNode list2){
         ListNode dummyHead= new ListNode();
-        ListNode cur=dummyHead;
+        ListNode head = dummyHead;
         while(list1!=null&&list2!=null){
             if(list1.val<list2.val){
-                cur.next=list1;
+                head.next=list1;
                 list1=list1.next;
             }else{
-                cur.next=list2;
+                head.next=list2;
                 list2=list2.next;
             }
-            cur=cur.next;
+            head=head.next;
         }
         if(list1==null){
-            cur.next=list2;
+            head.next=list2;
         }else{
-            cur.next=list1;
+            head.next=list1;
         }
         return dummyHead.next;
     }
     public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0){
+        if(lists==null||lists.length==0){
             return null;
         }
-        if(lists.length==1){
-            return lists[0];
-        }
-        for(int i=1; i<lists.length; i++){
-            lists[0]=mergeLists(lists[0],lists[i]);
+        for(int i=1;i<lists.length;i++){
+            lists[0]=merge2Lists(lists[0],lists[i]);
         }
         return lists[0];
         
