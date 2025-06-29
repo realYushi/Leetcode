7a5af8210fb2696d31957a8ab2720354e58177e2
Yushi Cui <qs8cys@gmail.com>
2025-06-29 18:49:30 +1200

    Time: 0 ms (100.00%) | Memory: 42.3 MB (11.27%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,41 +1,33 @@
 class Solution {
-    int[][] dp;
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if(obstacleGrid==null||obstacleGrid.length==0||obstacleGrid[0].length==0){
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1){
             return 0;
         }
-        int row=obstacleGrid.length;
-        int col=obstacleGrid[0].length;
-        if(obstacleGrid[0][0]==1||obstacleGrid[row-1][col-1]==1){
-            return 0;
-        }
-        dp=new int[row][col];
+        int[][]dp =new int[m][n];
         dp[0][0]=1;
-        for (int i = 1; i < row; i++) {
-            if (obstacleGrid[i][0] == 0 && dp[i - 1][0] == 1) {
-                dp[i][0] = 1;
-            } else {
-                dp[i][0] = 0;
+        for(int i = 1;i<m;i++){
+            if(obstacleGrid[i][0]==0){
+                dp[i][0]=dp[i-1][0];
             }
         }
+        for(int i = 1;i<n;i++){
+            if(obstacleGrid[0][i]==0){
+                dp[0][i]=dp[0][i-1];
+            }
+        }
+        for(int r=1;r<m;r++){
+            for(int c=1; c<n;c++){
+                if(obstacleGrid[r][c]==1){
+                    dp[r][c]=0;
+                }else{
+                    dp[r][c]=dp[r-1][c]+dp[r][c-1];
+                }
+            }
+        }
+        return dp[m-1][n-1];
 
-        for (int j = 1; j < col; j++) {
-            if (obstacleGrid[0][j] == 0 && dp[0][j - 1] == 1) {
-                dp[0][j] = 1;
-            } else {
-                dp[0][j] = 0;
-            }
-        }
-        for(int r=1; r<row; r++){
-           for(int c=1; c<col; c++){
-            dp[r][c]=dp[r-1][c]+dp[r][c-1];
-
-            if(obstacleGrid[r][c]==1){
-                dp[r][c]=0;
-            }
-           } 
-        }
-        return dp[row-1][col-1];
         
     }
 }
