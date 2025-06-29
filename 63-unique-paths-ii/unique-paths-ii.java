7d7173065ba906c7d06f671a6a5511429f81537c
Yushi Cui <qs8cys@gmail.com>
2025-06-29 19:05:12 +1200

    Time: 0 ms (100.00%) | Memory: 41.7 MB (80.66%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -8,20 +8,20 @@ class Solution {
         if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1){
             return 0;
         }
-        int[][] dp= new int[m][n];
+        int[][] dp= new int [m][n];
         dp[0][0]=1;
-        for(int i=1;i<m;i++){
+        for(int i = 1;i<m;i++){
             if(obstacleGrid[i][0]==0){
                 dp[i][0]=dp[i-1][0];
             }
         }
-        for(int i=1;i<n;i++){
+        for(int i = 1;i<n;i++){
             if(obstacleGrid[0][i]==0){
                 dp[0][i]=dp[0][i-1];
             }
         }
-        for(int r=1 ; r<m;r++){
-            for(int c=1; c<n;c++){
+        for(int r=1; r<m;r++){
+            for(int c=1; c<n; c++){
                 if(obstacleGrid[r][c]==1){
                     dp[r][c]=0;
                 }else{
@@ -30,7 +30,6 @@ class Solution {
             }
         }
         return dp[m-1][n-1];
-
         
     }
 }
