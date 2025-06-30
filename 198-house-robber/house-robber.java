672525e3bf712bcb393e0ae8f468dba6f20962cf
Yushi Cui <qs8cys@gmail.com>
2025-06-30 14:00:08 +1200

    Time: 0 ms (100.00%) | Memory: 41.2 MB (46.91%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,17 +3,16 @@ class Solution {
     public int rob(int[] nums) {
         dp=new int[nums.length];
         Arrays.fill(dp,-1);
-        return dsf(nums,0);
+        return dfs(nums,0);
     }
-    private int dsf(int[] nums, int i){
+    private int dfs(int[] nums, int i){
         if(i>=nums.length){
             return 0;
         }
         if(dp[i]!=-1){
             return dp[i];
         }
-        dp[i]=Math.max(nums[i]+dsf(nums,i+2),dsf(nums,i+1));
+        dp[i]=Math.max(nums[i]+dfs(nums,i+2),dfs(nums,i+1));
         return dp[i];
-
     }
 }
