3a8b60723779591cd3df73fa8ce5d236bd6df23f
Yushi Cui <qs8cys@gmail.com>
2025-07-01 20:40:46 +1200

    Time: 0 ms (100.00%) | Memory: 40.9 MB (42.93%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -2,13 +2,10 @@ class Solution {
     public int hammingWeight(int n) {
         int res=0;
         while(n!=0){
-            if((n&1)==1){
-                res++;
-            }
-            n=n>>1;
+            n&=n-1;
+            res++;
         }
         return res;
-
         
     }
 }
