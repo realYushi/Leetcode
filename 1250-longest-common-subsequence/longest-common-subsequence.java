85803b9d9033a6746a1794f476569bc925ece1f0
Yushi Cui <qs8cys@gmail.com>
2025-07-03 11:13:35 +1200

    Time: 20 ms (74.12%) | Memory: 50.8 MB (77.00%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,9 +1,9 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m=text1.length();
-        int n= text2.length();
+        int n=text2.length();
         int[][] dp= new int[m+1][n+1];
-        for(int r=1; r<=m;r++){
+        for(int r=1; r<=m; r++){
             for(int c=1; c<=n; c++){
                 if(text1.charAt(r-1)==text2.charAt(c-1)){
                     dp[r][c]=dp[r-1][c-1]+1;
@@ -12,7 +12,8 @@ class Solution {
                 }
             }
         }
-            return dp[m][n];
+        return dp[m][n]; 
+
         
     }
 }
