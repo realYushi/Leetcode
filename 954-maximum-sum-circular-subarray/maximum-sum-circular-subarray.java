b259a983660b9283170c9dae1642083aaf9fb955
Yushi Cui <qs8cys@gmail.com>
2025-07-05 11:10:11 +1200

    Time: 6 ms (76.65%) | Memory: 49.8 MB (11.39%) - LeetSync

--- 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
+++ 954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int gmax=nums[0],gmin=nums[0];
-        int min=0,max=0,t=0;
+        int gmax=nums[0], gmin=nums[0];
+        int max=0, min=0, sum=0;
         for(int n:nums){
-            t+=n;
-            min=Math.min(n,min+n);
-            gmin=Math.min(min,gmin);
+            sum+=n;
             max=Math.max(n,max+n);
             gmax=Math.max(max,gmax);
+            min=Math.min(n,min+n);
+            gmin=Math.min(min,gmin);
         }
-        return gmax<0?gmax:Math.max(gmax,t-gmin);
+        return gmax<0?gmax:Math.max(gmax,sum-gmin);
         
     }
 }
