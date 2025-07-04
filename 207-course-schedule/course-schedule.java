4f7424b794747e693ccebfb6b541f7a6b6dc4f9e
Yushi Cui <qs8cys@gmail.com>
2025-07-05 10:14:37 +1200

    Time: 7 ms (47.63%) | Memory: 46.2 MB (13.18%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -4,34 +4,34 @@ class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         map=new HashMap<>();
         set=new HashSet<>();
-        for(int i=0; i<numCourses; i++){
-            map.put(i, new LinkedList<>());
-        }
-        for(int[] pair:prerequisites ){
-            map.get(pair[0]).add(pair[1]);
-        }
         for(int i=0; i<numCourses;i++){
+            map.put(i,new ArrayList<>());
+        }
+        for(int[]rule : prerequisites){
+            map.get(rule[0]).add(rule[1]);
+        }
+        for(int i=0; i<numCourses; i++){
             if(!dfs(i)){
                 return false;
             }
         }
         return true;
     }
-    private boolean dfs(int i){
-        if(set.contains(i)){
+    private boolean dfs(int course){
+        if(set.contains(course)){
             return false;
         }
-        if(map.get(i).isEmpty()){
+        if(map.get(course).isEmpty()){
             return true;
         }
-        set.add(i);
-        for(int pre:map.get(i)){
+        set.add(course);
+        for(int pre:map.get(course)){
             if(!dfs(pre)){
                 return false;
             }
         }
-        map.put(i, new LinkedList<>());
-        set.remove(i);
+        map.put(course,new ArrayList<>());
+        set.remove(course);
         return true;
     }
 }
