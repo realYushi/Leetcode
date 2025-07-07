b16962610efe484a442760dcb7bc60a44b871ab4
Yushi Cui <qs8cys@gmail.com>
2025-07-07 23:22:03 +1200

    Time: 19 ms (38.85%) | Memory: 44.4 MB (96.57%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,29 +1,28 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         Set<Character> set = new HashSet<>();
-        for (char c : s.toCharArray()) {
+        for(char c:s.toCharArray()){
             set.add(c);
         }
-        int res = 0;
-        for (char c : set) {
-            int count = 0;
-            int l = 0;
-            for (int r = 0; r < s.length(); r++) {
-                if (s.charAt(r) == c) {
+        int res=0;
+        for(char c: set){
+            int l=0;
+            int count=0;
+            for(int r=0; r<s.length();r++){
+                if(s.charAt(r)==c){
                     count++;
-
                 }
-                while ((r - l + 1) - count > k) {
-                    if (s.charAt(l) == c) {
+                while(r-l+1-count>k){
+                    if(s.charAt(l)==c){
                         count--;
                     }
                     l++;
                 }
-
-                res = Math.max(res, r - l + 1);
+                res=Math.max(res,r-l+1);
             }
+
         }
         return res;
-
+        
     }
 }
