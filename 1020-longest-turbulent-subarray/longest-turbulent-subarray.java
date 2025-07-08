eed5c2725ac4248d797d8c484a159f154f951f5e
Yushi Cui <qs8cys@gmail.com>
2025-07-09 10:48:51 +1200

    Time: 6 ms (80.12%) | Memory: 48.7 MB (92.64%) - LeetSync

--- 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
+++ 1020-longest-turbulent-subarray/longest-turbulent-subarray.java
@@ -1,7 +1,9 @@
 class Solution {
     public int maxTurbulenceSize(int[] arr) {
-        int inc=1, dec=1, res=1;
-        for( int i=1; i<arr.length; i++){
+        int inc=1;
+        int dec=1;
+        int res=1;
+        for(int i=1; i<arr.length; i++){
             if(arr[i-1]<arr[i]){
                 inc=dec+1;
                 dec=1;
@@ -12,7 +14,7 @@ class Solution {
                 dec=1;
                 inc=1;
             }
-            res=Math.max(res,Math.max(inc,dec));
+            res=Math.max(res,Math.max(dec,inc));
         }
         return res;
         
