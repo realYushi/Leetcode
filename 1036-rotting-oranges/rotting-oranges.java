bb06d8423770b69146db0ac8bbce38ae1e68a9f1
Yushi Cui <qs8cys@gmail.com>
2025-07-13 17:41:12 +1200

    Time: 1 ms (100.00%) | Memory: 42.3 MB (75.69%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,38 +1,43 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int freash =0;
+        int ROW=grid.length;
+        int COL=grid[0].length;
+        int fresh=0;
         Queue<int[]> q= new LinkedList<>();
-        for(int r=0; r<grid.length;r++){
-            for(int c=0; c<grid[0].length;c++){
+
+        for( int r= 0; r<ROW;r++){
+            for(int c=0; c<COL;c++){
                 if(grid[r][c]==1){
-                    freash++;
+                    fresh++;
                 }
-                if(grid[r][c]==2){
+                else if(grid[r][c]==2){
                     q.add(new int[]{r,c});
                 }
+
             }
         }
-        int[][] dir = new int[][]{
-            {0,1},{0,-1},{1,0},{-1,0}
-        };
+
         int turn=0;
-        while(freash>0&&!q.isEmpty()){
-            int len=q.size();
-            for(int i=0; i<len; i++){
-                int[] p=q.remove();
-                for(int[] d:dir){
+        int[][] dirs= new int[][]{
+            {0,1},{1,0},{-1,0},{0,-1}
+        };
+        while(fresh!=0&&!q.isEmpty()){
+            int len= q.size();
+            for(int i=0; i<len;i++){
+                int[] p= q.remove();
+                for(int[] d :dirs ){
                     int nr=p[0]+d[0];
                     int nc=p[1]+d[1];
-                    if(nr>=0&&nc>=0&&nr<grid.length&&nc<grid[0].length&&grid[nr][nc]==1){
-                        freash--;
-                        grid[nr][nc]=2;
+                    if(nr>=0&&nc>=0&&nr<ROW&&nc<COL&&grid[nr][nc]==1){
                         q.add(new int[]{nr,nc});
+                        grid[nr][nc]=2;
+                        fresh--;
                     }
                 }
             }
             turn++;
         }
-        return freash==0?turn:-1;
+        return fresh==0?turn:-1;
         
     }
 }
