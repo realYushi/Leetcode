8d80247192cb56cef3559ebaf4ee54bfd4e9137e
Yushi Cui <qs8cys@gmail.com>
2025-07-14 22:16:01 +1200

    Time: 0 ms (100.00%) | Memory: 42.2 MB (97.04%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,10 +10,13 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head==null||head.next==null){
+        if(head==null){
             return head;
         }
-        ListNode newHead = reverseList(head.next);
+        if(head.next==null){
+            return head;
+        }
+        ListNode newHead= reverseList(head.next);
         head.next.next=head;
         head.next=null;
         return newHead;
