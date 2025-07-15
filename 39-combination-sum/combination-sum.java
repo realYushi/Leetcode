cf044d45f69c2c558966ab89a8bfb76851be0d40
Yushi Cui <qs8cys@gmail.com>
2025-07-15 16:55:37 +1200

    Time: 3 ms (22.36%) | Memory: 44.7 MB (64.34%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res= new LinkedList<>();
+        List<List<Integer>> list= new LinkedList<>();
         List<Integer> sub= new LinkedList<>();
-        dfs(res,sub,candidates,target,0);
-        return res;
+        dfs(list,sub,candidates,target,0);
+        return list;
     }
-    private void dfs(List<List<Integer>> res, List<Integer> sub, int[] candidates, int target, int index){
-        if(index>=candidates.length || target<0){
+    private void dfs(List<List<Integer>> list, List<Integer> sub,int[] candidates, int target, int index){
+        if(index>=candidates.length||target<0){
             return;
         }
         if(target==0){
-            res.add(new LinkedList<>(sub));
+            list.add(new LinkedList<>(sub));
             return;
         }
         sub.add(candidates[index]);
-        dfs(res,sub,candidates,target-candidates[index],index);
+        dfs(list,sub,candidates,target-candidates[index],index);
         sub.remove(sub.size()-1);
-        dfs(res,sub,candidates,target,index+1);
+        dfs(list,sub,candidates,target,index+1);
+
 
     }
 }
