10ed90f10a1724d1a663aa87c836af91590150ef
Yushi Cui <qs8cys@gmail.com>
2025-07-15 22:07:22 +1200

    Time: 0 ms (100.00%) | Memory: 41.4 MB (96.80%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,11 +1,12 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        int m= obstacleGrid.length;
-        int n=obstacleGrid[0].length;
-        if(obstacleGrid[0][0]==1||obstacleGrid[m-1][n-1]==1){
+        int m=obstacleGrid.length;
+        int n= obstacleGrid[0].length;
+        if(obstacleGrid[m-1][n-1]==1||obstacleGrid[0][0]==1){
             return 0;
         }
-        int[][] dp = new int [m][n];
+        
+        int[][] dp = new int[m][n];
         dp[0][0]=1;
         for(int i=1; i<m; i++){
             if(obstacleGrid[i][0]!=1){
@@ -17,17 +18,17 @@ class Solution {
                 dp[0][i]=dp[0][i-1];
             }
         }
-        for(int r=1;r<m; r++){
+        for(int r=1; r<m; r++){
             for(int c=1; c<n; c++){
                 if(obstacleGrid[r][c]==1){
                     dp[r][c]=0;
                 }else{
 
-                dp[r][c]=dp[r-1][c]+dp[r][c-1];
+                dp[r][c]= dp[r-1][c]+dp[r][c-1];
                 }
             }
         }
-        return dp[m-1][n-1];
+        return dp[m-1][n-1]; 
         
     }
 }
