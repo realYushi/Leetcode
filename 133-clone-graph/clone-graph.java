b29dcbaaa2839366007ac5d471021b3b023c55ec
Yushi Cui <qs8cys@gmail.com>
2025-07-15 16:44:10 +1200

    Time: 26 ms (49.80%) | Memory: 42.8 MB (53.97%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -21,21 +21,21 @@ class Node {
 class Solution {
     Map<Node,Node> map;
     public Node cloneGraph(Node node) {
-        map= new HashMap<>();
-        return dfs(node);
-    }
-    private Node dfs(Node node){
+        map=new HashMap<>();
         if(node==null){
             return node;
         }
-        if(map.containsKey(node)){
+        return dfs(node);
+    }
+    private Node dfs(Node node){
+        if(!map.containsKey(node)){
+            map.put(node,new Node(node.val));
+        }else{
             return map.get(node);
         }
-        map.put(node,new Node(node.val));
         for(Node nei:node.neighbors){
             map.get(node).neighbors.add(dfs(nei));
         }
         return map.get(node);
-
     }
 }
