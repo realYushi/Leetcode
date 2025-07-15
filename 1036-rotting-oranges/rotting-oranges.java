55488c31aaf5df7236a2cccc5d4907853aa1bb6f
Yushi Cui <qs8cys@gmail.com>
2025-07-15 17:09:12 +1200

    Time: 2 ms (67.81%) | Memory: 42.7 MB (29.54%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,8 +1,9 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int ROW= grid.length;
-        int COL= grid[0].length;
+        int ROW=grid.length;
+        int COL=grid[0].length;
         int fresh=0;
+        int round=0;
         Queue<int[]> q= new LinkedList<>();
         for(int r=0; r<ROW; r++){
             for(int c=0; c<COL; c++){
@@ -13,18 +14,16 @@ class Solution {
                 }
             }
         }
-
         int[][] dirs= new int[][]{
-            {0,1},{0,-1},{1,0},{-1,0}
+            {0,1},{1,0},{0,-1},{-1,0}
         };
-        int counter=0;
-        while(fresh!=0&&!q.isEmpty()){
-            int len=q.size();
-            for(int i=0;i<len;i++){
+        while(fresh>0&&!q.isEmpty()){
+            int len= q.size();
+            for(int i=0; i<len; i++){
                 int[] p= q.remove();
                 for(int[] d:dirs){
-                    int nr=p[0]+d[0];
-                    int nc=p[1]+d[1];
+                    int nr= p[0]+d[0];
+                    int nc= p[1]+d[1];
                     if(nr>=0&&nc>=0&&nr<ROW&&nc<COL&&grid[nr][nc]==1){
                         q.add(new int[]{nr,nc});
                         fresh--;
@@ -32,10 +31,9 @@ class Solution {
                     }
                 }
             }
-            counter++;
+            round++;
         }
-        return fresh==0?counter:-1;
-        
+        return fresh==0?round:-1;
         
     }
 }
